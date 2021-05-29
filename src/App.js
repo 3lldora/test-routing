05084057c46b8ae1892be280d0d6db4b80b63d08
ellen weng <ellen@pandorabots.com>
2021-05-29 09:12:11 -0700

    initial commit"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Route, Link, Switch} from "react-router-dom";
+import React from 'react'
+
+function Home(props){
+    return <h1>Home Comp</h1>
+
+}
+function About(props){
+    return <h1>About Comp</h1>
+
+}
+function Chat(props){
+    return <h1>Chat Comp</h1>
+
+}
 
 function App() {
+  const handleClick = (event)=>{
+    event.preventDefault()
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <div className="App">
+        <header className="App-header">
+
+          <BrowserRouter>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/chat">Chat</Link>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/chat">
+              <Chat />
+            </Route>
+            {/*<Route exact path="/" render={()=> <h1>My Home</h1>}> </HomeRoute>*/}
+            {/*<Route path="/about" component={About}> </Route>*/}
+            {/*<Route path="/chat" component={Chat}> </Route>*/}
+          </BrowserRouter>
+        </header>
+      </div>
+
+
   );
 }
 
